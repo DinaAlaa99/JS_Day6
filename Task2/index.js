f406b2f86be9427dc9bb3ea32ee809e72ee8e1d8
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-02-10 15:21:43 +0200

    fix local storage array

--- Task2/index.js
+++ Task2/index.js
@@ -1,53 +1,24 @@
-var myStorage = [];
+var todoForm = document.getElementById("myDIV");
+var todoInput = document.getElementById("myInput");
+var todoItems = document.getElementById("myUL");
+
 
 //append the close button to the list
-var myNodelist = document.getElementsByTagName("LI");
-for (var i = 0; i < myNodelist.length; i++) {
-    var span = makeCloseButton();
-    myNodelist[i].appendChild(span);
+function appendClose() {
+    var myNodelist = document.getElementsByTagName("LI");
+    for (var i = 0; i < myNodelist.length; i++) {
+        var span = makeCloseButton();
+        myNodelist[i].appendChild(span);
+    }
 }
 
 //append the check button to the list
-var myNodelist = document.getElementsByTagName("LI");
-for (var i = 0; i < myNodelist.length; i++) {
-    var span = makeCheckButton();
-    myNodelist[i].appendChild(span);
-   
-}
-
-var close = document.getElementsByClassName("close");
-closeItem();
-
-var check = document.getElementsByClassName("check");
-checkItem();
-
-// "Add" function
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-   
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-        myStorage.push(inputValue);
-        displayStorge();
-
+function appendCheck() {
+    var myNodelist = document.getElementsByTagName("LI");
+    for (var i = 0; i < myNodelist.length; i++) {
+        var span = makeCheckButton();
+        myNodelist[i].appendChild(span);
     }
-    document.getElementById("myInput").value = "";
-
-
-    var span = makeCloseButton();
-    li.appendChild(span);
-
-
-    var span = makeCheckButton();
-    li.appendChild(span);
-
-    closeItem();
-    checkItem();
 }
 
 function makeCloseButton() {
@@ -58,15 +29,6 @@ function makeCloseButton() {
     return span;
 }
 
-function closeItem() {
-    for (var i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
-
 function makeCheckButton() {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u2713");
@@ -75,19 +37,101 @@ function makeCheckButton() {
     return span;
 }
 
-function checkItem() {
-    for (var i = 0; i < check.length; i++) {
-        check[i].onclick = function (ev) {
-            var div = this.parentElement;
-            div.classList.toggle('checked');
-        }
+
+var todos = [];
+
+function newElement() {
+    var todoInput = document.getElementById("myInput");
+    addTodo(todoInput.value);
+}
+
+// function to add todo
+function addTodo(item) {
+    if (item !== "") {
+        const todo = {
+            id: Date.now(), //to help check and delete
+            name: item,
+            completed: false
+        };
+
+        todos.push(todo);
+        addToLocalStorage(todos);
+        todoInput.value = "";
+    } else {
+        alert("You must write something to add");
     }
 }
 
-function displayStorge()
-{
-    for (var i = 0; i < myStorage.length; i++)
-    {
-        console.log(myStorage[i]);
+function addToLocalStorage(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    displayTodos(todos);
+
+}
+
+function getFromLocalStorage() {
+    const reference = localStorage.getItem('todos');
+    if (reference) {
+        todos = JSON.parse(reference);
+        displayTodos(todos);
     }
 }
+
+getFromLocalStorage();
+
+
+function displayTodos(todos) {
+  
+todoItems.innerHTML = '';
+
+    todos.forEach(function (item) {
+        const checked = item.completed ? 'checked' : null;
+
+        var li = document.createElement('li');
+        var inputValue = document.getElementById("myInput");
+        var t = document.createTextNode(inputValue);
+
+        li.setAttribute('data-key', item.id); //to check or delete
+
+        if (item.completed === true) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = `${item.name}`;
+        console.log(item.name)
+
+        // finally add the <li> to the <ul>
+        todoItems.append(li);
+
+    });
+    appendClose();
+    appendCheck();
+}
+
+function deleteItem(id) {
+    todos = todos.filter(function (item) {
+        return item.id != id;
+    });
+    addToLocalStorage(todos);
+}
+
+function checkItem(id) {
+    todos.forEach(function (item) {
+        if (item.id == id) {
+            item.completed = !item.completed;
+        }
+    });
+    addToLocalStorage(todos);
+}
+
+
+todoItems.addEventListener('click', function (event) {
+    // check if the event is on check button
+    if (event.target.classList.contains('check')) {
+        checkItem(event.target.parentElement.getAttribute('data-key'));
+    }
+
+    // check if that is a delete button
+    if (event.target.classList.contains('close')) {
+        deleteItem(event.target.parentElement.getAttribute('data-key'));
+    }
+});
